a478479172fe13edd9fea2449ae502e78526adef
Юлия Глухова <juliaglukhova666@gmail.com>
2021-09-07 22:38:44 +0400

    main page done

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const path = require('path');
-
+const hbs = require('hbs');
 const indexRouter = require('./routes/index.router');
 
 const app = express();
@@ -13,6 +13,7 @@ app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+hbs.registerPartials(path.join(process.env.PWD, 'src/views/partials'));
 
 app.use('/', indexRouter);
 
