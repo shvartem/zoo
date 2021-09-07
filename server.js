ca183f4ad78f43f275fd7f062880c193822f1a8c
Юлия Глухова <juliaglukhova666@gmail.com>
2021-09-07 16:12:39 +0400

    test

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require('./src/app');
 
 const PORT = process.env.PORT ?? 3000;
 
+console.log('test');
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
 });
