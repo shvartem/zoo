699a7dd82c393ae261daba9d15d0d68fdb3f9be7
shvartem <shvartem422@yandex.ru>
2021-09-07 18:00:30 +0500

    created models for the DB

---
+++ src/db/migrations/20210907122721-create-animal.js
@@ -0,0 +1,38 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Animals', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+        validate: {
+          isAlpha: true,
+        },
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+        validate: {
+          isAlpha: true,
+        },
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'Categories',
+          },
+        },
+      },
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('Animals');
+  },
+};
---
+++ src/db/models/admin.js
@@ -0,0 +1,49 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Admin extends Model {
+    static associate() {}
+  }
+  Admin.init(
+    {
+      firstName: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+          isAlpha: true,
+        },
+      },
+      lastName: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+          isAlpha: true,
+        },
+      },
+      email: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+          len: [6, 10],
+        },
+      },
+      isSuperAdmin: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      timestamps: false,
+      modelName: 'Admin',
+    },
+  );
+  return Admin;
+};
