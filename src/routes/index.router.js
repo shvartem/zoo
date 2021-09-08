dd325c4813134c327d412cd6290e9d20087933d0
Юлия Глухова <juliaglukhova666@gmail.com>
2021-09-08 12:15:53 +0400

    front

--- src/routes/index.router.js
+++ src/routes/index.router.js
@@ -6,5 +6,18 @@ router.get('/', (req, res) => {
   res.render('index')
 })
 
+router.get('/news', (req, res) => {
+  res.render('news')
+})
+
+router.get('/categories/1', (req, res) => {
+  res.render('category')
+})
+
+
+router.get('/categories/1/animals/1', (req, res) => {
+  res.render('animal')
+})
+
 
 module.exports = router;
