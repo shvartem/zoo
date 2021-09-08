f4dc8fd93ab521b30c2111f50b9e54980d9a645a
Юлия Глухова <juliaglukhova666@gmail.com>
2021-09-08 22:28:07 +0400

    galery done

--- src/routes/index.router.js
+++ src/routes/index.router.js
@@ -2,14 +2,14 @@ const router = require('express').Router();
 
 const NewsService = require('../services/news.service')
 const TariffsService = require('../services/tarifs.service');
+const PhotosService = require('../services/photos.service')
 
 router.get('/', async (req, res) => {
   const news = await NewsService.findLastNews();
   const tariffs = await TariffsService.getAllTarifs()
-  console.log(tariffs);
-  res.render('index', { categories: req.categories, news, tariffs });
+  const images = await PhotosService.getAllPhotos()
+  console.log(images);
+  res.render('index', { categories: req.categories, news, tariffs, images });
 });
-
-
 
 module.exports = router;
