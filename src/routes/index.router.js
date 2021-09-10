cd6d3e94ea255890b39f300905574d6953ed07ec
shvartem <shvartem422@yandex.ru>
2021-09-10 11:59:24 +0500

    added edit schedule form

--- src/routes/index.router.js
+++ src/routes/index.router.js
@@ -3,15 +3,24 @@ const router = require('express').Router();
 const NewsService = require('../services/news.service');
 const TariffsService = require('../services/tarifs.service');
 const PhotosService = require('../services/photos.service');
+const SchedulesService = require('../services/schedule.service');
 
 router.get('/', async (req, res) => {
   const news = await NewsService.findLastNews();
   const tariffs = await TariffsService.getAllTarifs();
   const images = await PhotosService.getAllPhotos();
-  if(req.session?.admin?.name) {
-    const {admin} = req.session
-    res.render('index', { categories: req.categories, news, tariffs, images, admin });
-    return
+  const schedule = await SchedulesService.getSchedule();
+  if (req.session?.admin?.name) {
+    const { admin } = req.session;
+    res.render('index', {
+      categories: req.categories,
+      news,
+      tariffs,
+      images,
+      admin,
+      schedule,
+    });
+    return;
   }
   res.render('index', { categories: req.categories, news, tariffs, images });
 });
---
+++ src/services/schedule.service.js
@@ -0,0 +1,53 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable class-methods-use-this */
+const db = require('../db/models');
+const { sequelize } = require('../db/models');
+
+class SchedulesService {
+  static async getSchedule() {
+    try {
+      const schedule = await db.Schedule.findOne();
+      for (const key in schedule.dataValues) {
+        if (
+          schedule.dataValues.hasOwnProperty(key) &&
+          typeof schedule.dataValues[key] === 'string'
+        ) {
+          schedule.dataValues[key] = schedule.dataValues[key].slice(
+            0,
+            schedule.dataValues[key].length - 3,
+          );
+        }
+      }
+      console.log(schedule);
+      return schedule;
+    } catch (e) {
+      console.error(e);
+      return { message: 'Не удалось найти расписание.' };
+    }
+  }
+
+  async editSchedule({
+    weekdaysOpening,
+    weekdaysClosing,
+    weekendOpening,
+    weekendClosing,
+  }) {
+    try {
+      const schedule = await db.Schedule.update(
+        { weekdaysOpening, weekdaysClosing, weekendOpening, weekendClosing },
+        {
+          where: {
+            id: 1,
+          },
+        },
+      );
+
+      return schedule;
+    } catch (e) {
+      console.error(e);
+      return { message: 'Не удалось изменить расписание.' };
+    }
+  }
+}
+
+module.exports = SchedulesService;
